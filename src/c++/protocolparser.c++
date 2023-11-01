62706e01329095472d9674f3d70b8698bcd9c182
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-01 23:49:09 +0300

    ...

---
+++ src/c++/protocolparser.c++
@@ -0,0 +1,12 @@
+/**
+  *  @file FILE
+  *  @author Dmitry Ryazancev
+  *  @date 01.11.23
+  *  @copyright Radar-MMS 2023
+  */
+
+#include "protocolparser.h"
+
+namespace HSA
+{
+} // HSA
---
+++ src/c++/protocolparser.h
@@ -0,0 +1,17 @@
+/**
+  *  @file FILE
+  *  @author Dmitry Ryazancev
+  *  @date 01.11.23
+  *  @copyright Radar-MMS 2023
+  */
+
+#pragma once
+
+#include <vt45.h>
+
+namespace HSA
+{
+  class ProtocolParser
+  {
+  };
+} // HSA
