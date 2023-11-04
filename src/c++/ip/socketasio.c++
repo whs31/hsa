cfd5031b3fac3fd2befc6ee6f97ae35583c44c31
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-04 18:30:16 +0300

    Will finish later

--- src/c++/ip/socketasio.c++
+++ src/c++/ip/socketasio.c++
@@ -3,3 +3,18 @@
 //
 
 #include "socketasio.h"
+
+namespace HSA
+{
+//  SocketASIO::SocketASIO(ISocket::SocketReceiveCallback callback)
+//    : m_context()
+//    , m_socket(m_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), 1111))
+//    , m_callback(std::move(callback))
+//    , m_buffer({})
+//  {}
+//
+//  void SocketASIO::start(u16 port) noexcept
+//  {
+//    m_socket.
+//  }
+} // HSA
--- src/c++/ip/socketasio.h
+++ src/c++/ip/socketasio.h
@@ -2,14 +2,36 @@
 // Created by whs31 on 04.11.2023.
 //
 
-#ifndef HSA_SOCKETASIO_H
-#define HSA_SOCKETASIO_H
+#pragma once
 
+#include "ip/isocket.h"
+#include <array>
+#include <asio/ip/udp.hpp>
 
-class SocketASIO
+using std::array;
+
+namespace HSA
 {
+  class SocketASIO : public ISocket
+  {
+    public:
+      explicit SocketASIO(SocketReceiveCallback callback);
+      ~SocketASIO();
 
-};
+      void start(u16 port) noexcept override;
+      void stop() noexcept override;
+      void send(const string& data) noexcept override;
+      void joinMulticastGroup(const string& ip) noexcept override;
 
+    protected:
+      void read() noexcept override;
 
-#endif //HSA_SOCKETASIO_H
+    protected:
+      SocketReceiveCallback m_callback;
+//      asio::io_context m_context;
+//      asio::ip::udp::socket m_socket;
+//      asio::ip::udp::endpoint m_endpoint;
+//      array<char, 1024> m_buffer;
+  };
+} // HSA
+
